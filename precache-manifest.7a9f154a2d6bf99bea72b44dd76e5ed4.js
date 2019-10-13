8fcb8051d0e400a270829d4d2f1a9bf853209676
artoniaz <artoni.az@gmail.com>
2019-10-13 19:58:14 +0200

    Updates

---
+++ precache-manifest.7a9f154a2d6bf99bea72b44dd76e5ed4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e55a27c11d6602c71c46c8363e62f81",
+    "url": "/Full-Football-Database/index.html"
+  },
+  {
+    "revision": "9e9e5cfb238efcd9ff61",
+    "url": "/Full-Football-Database/static/css/main.0b0ce94a.chunk.css"
+  },
+  {
+    "revision": "7ec5a1eb781b267777b2",
+    "url": "/Full-Football-Database/static/js/2.5379f705.chunk.js"
+  },
+  {
+    "revision": "9e9e5cfb238efcd9ff61",
+    "url": "/Full-Football-Database/static/js/main.d9524920.chunk.js"
+  },
+  {
+    "revision": "592726bfd5ebf58a74f4",
+    "url": "/Full-Football-Database/static/js/runtime~main.1d24f923.js"
+  },
+  {
+    "revision": "c8499801f281f05fbad76c3c9216100f",
+    "url": "/Full-Football-Database/static/media/ball.c8499801.png"
+  },
+  {
+    "revision": "c5ed62472775b974787720b3830f01fb",
+    "url": "/Full-Football-Database/static/media/grass-114651_1920.c5ed6247.jpg"
+  },
+  {
+    "revision": "db31a7507c26192c1ddc3d35972fc3a3",
+    "url": "/Full-Football-Database/static/media/stadium.db31a750.jpg"
+  }
+]);
